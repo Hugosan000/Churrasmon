825aaf4b5920f14e4cf7769140fdd73c289c7646
Hugosan000 <victorfonsecasp@gmail.com>
2022-06-11 00:01:22 -0300

    sexo quente

--- cello.py
+++ cello.py
@@ -1,1 +1,22 @@
-#test de push
+
+import pygame
+
+from pygame.locals import *
+
+from sys import exit
+
+pygame.init()
+
+largura = 640
+
+altura = 480
+
+tela = pygame.display.set_mode((largura, altura))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()       
+            exit()
+            
+pygame.display.update()
